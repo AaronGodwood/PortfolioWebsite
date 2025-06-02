c0500551c6596f28e9b0308d1a97832a881481b8
Aaron Godwood <godwoodaaron@gmail.com>
2025-06-02 15:24:41 +0100

    minor tweaks

--- src/assets/NavBar.jsx
+++ src/assets/NavBar.jsx
@@ -3,7 +3,7 @@ import {FaGithub, FaInstagram, FaLinkedin} from "react-icons/fa";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 w-full h-20 bg-white text-black flex justify-between items-center px-8 py-4 z-50 shadow-md backdrop-blur">
+        <nav className="fixed top-0 left-0 w-full h-20 bg-white/80 text-black flex justify-between items-center px-8 py-4 z-50 shadow-md backdrop-blur">
             {/* Left: Name/Logo */}
             <div className="flex items-center space-x-3 px-10">
                 <img src={"./images/logoDark.png"} alt="Logo" className="w-12 h-12 object-contain" />
